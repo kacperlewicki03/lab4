ca7e6a0825582eda8f5d278a72eea40ea951a9cb
Kacper Lewicki <kacperlewicki@Kacpers-MBP.T-Mobile>
2024-06-05 21:15:59 +0200

    Dodanie flagi --logs do skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-if [[ "$1" == "--date" ]]; then
-    date
-fi
+case "$1" in
+    --date)
+        date
+        ;;
+    --logs)
+        num=${2:-100}
+        for i in $(seq 1 $num); do
+            filename="log$i.txt"
+            echo "Nazwa pliku: $filename" > $filename
+            echo "Utworzony przez: skrypt.sh" >> $filename
+            echo "Data: $(date)" >> $filename
+        done
+        ;;
+    --help)
+        echo "Dostępne opcje:"
+        echo "--date        - Wyświetla dzisiejszą datę"
+        echo "--logs [num]  - Tworzy pliki log"
+        echo "--help        - Wyświetla pomoc"
+        ;;
+esac
